521b1bb54802e94dc2e4612d5f846983f5884320
Admin <lu.an.ri91@gmail.com>
2025-05-08 22:49:57 -0300

    modificando urls

--- rh_project/urls.py
+++ rh_project/urls.py
@@ -1,11 +1,8 @@
-from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.contrib import admin
+from django.urls import path
+
+
 
 urlpatterns = [
-    path('api/', include('rh_app.urls')),  # já deve estar aqui
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', admin.site.urls),
 ]
