f254d9d09bf466758f6f260a298dbe258e7036af
Admin <lu.an.ri91@gmail.com>
2025-05-08 22:51:10 -0300

    modificando admin

--- rh_app/admin.py
+++ rh_app/admin.py
@@ -1,24 +1,111 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Usuario #Cargo, Funcionario, Departamentos
-from django.contrib.auth.forms import UserChangeForm
+from .models import *
 
-class CustomUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
-        model = Usuario
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
 
-class UsuarioAdmin(UserAdmin):
-    form = CustomUserChangeForm
-    list_display = ('username', 'email', 'first_name', 'last_name', 'tipo_usuario', 'is_active')
-    list_filter = ('tipo_usuario', 'is_active')
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'email', 'telefone')}),
-        ('Permissões', {
-            'fields': ('tipo_usuario', 'is_active', 'is_staff', 'is_superuser', 
-                      'groups', 'user_permissions'),
-        }),
-        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
-    )
+    list_display = ('n_registro', 'nome', 'data_nascimento', 'telefone')
+    list_filter = ('nome',)
+    list_display_links = ('n_registro', 'nome')
+    search_fields = ('n_registro', 'nome', 'telefone')
 
-admin.site.register(Usuario, UsuarioAdmin)
+
+
+
+@admin.register(Empregador)
+class EmpregadorAdmin(admin.ModelAdmin):
+
+    list_display = ('n_registrofun', 'nome_emp', 'nomefunc', 'cnpj')
+    list_filter = ('nome_emp', 'nomefunc', 'cnpj')
+    list_display_links = ('nomefunc', 'nome_emp')
+    search_fields = ('nomefunc', 'nome_emp', 'cnpj')
+
+
+
+@admin.register(Residencia)
+class ResidenciaAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'endereco', 'cep')
+    list_filter = ('nomefunc', 'endereco', 'cep')
+    list_display_links = ('nomefunc',)
+    search_fields = ('nomefunc', 'endereco', 'cep')
+
+
+
+@admin.register(DocumentoFun)
+class DocumentoFunAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'rg', 'cpf', 'ctps', 'cnh')
+    list_filter = ('nomefunc', 'rg', 'cpf')
+    list_display_links = ('nomefunc',)
+    search_fields = ('nomefunc', 'rg', 'cpf', 'ctps', 'cnh')
+
+
+
+@admin.register(DocumentoEstrangeiro)
+class DocumentoEstrangeiroAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'rne', 'data_chegada', 'vencimento')
+    list_filter = ('nomefunc', 'rne')
+    list_display_links = ('nomefunc', 'rne')
+    search_fields = ('nomefunc', 'rne')
+
+
+
+@admin.register(Funcao)
+class FucaoAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'cargo', 'salario')
+    list_filter = ('nomefunc', 'cargo', 'salario')
+    list_display_links = ('nomefunc', 'cargo')
+    search_fields = ('nomefunc', 'cargo', 'salario')
+
+
+
+@admin.register(HorarioTrab)
+class HorarioTrabAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab')
+    list_filter = ('nomefunc',)
+    list_display_links = ('nomefunc',)
+    search_fields = ('nomefunc',)
+
+
+
+@admin.register(Beneficiario)
+class BeneficiarioAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'nome_ben', 'parentesco')
+    list_filter = ('nomefunc', 'nome_ben')
+    list_display_links = ('nomefunc', 'nome_ben')
+    search_fields = ('nomefunc', 'nome_ben')
+
+
+
+@admin.register(FGTS)
+class FGTSAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'banco_depositario', 'data_opcao')
+    list_filter = ('nomefunc', 'banco_depositario')
+    list_display_links = ('nomefunc',)
+    search_fields = ('nomefunc', 'banco_depositario')
+
+
+
+@admin.register(PIS)
+class PISAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'numero', 'nome_banco')
+    list_filter = ('nomefunc', 'numero', 'nome_banco')
+    list_display_links = ('nomefunc', 'numero')
+    search_fields = ('nomefunc', 'numero', 'nome_banco')
+
+
+
+@admin.register(Recisao)
+class RecisaoAdmin(admin.ModelAdmin):
+
+    list_display = ('nomefunc', 'numero_homologacao', 'data_saida', 'tipo_desligamento')
+    list_filter = ('nomefunc', 'numero_homologacao', 'data_saida', 'tipo_desligamento')
+    list_display_links = ('nomefunc', 'numero_homologacao')
+    search_fields = ('nomefunc', 'numero_homologacao', 'data_saida', 'tipo_desligamento')
